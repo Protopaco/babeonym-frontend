76a7bb1bffa5f860d58337a35f60cb62670cdace
Paul Stevens <paul.stevens.developer@gmail.com>
2026-03-07 23:06:08 -0800

    adds auto-padding to NameEvaluator component

---
+++ src/components/NameEvaluator/NameEvaluator.tsx
@@ -0,0 +1,40 @@
+import { useGivenNames, useGivenNamesActions } from '@/state/givenName/givenName.provider';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import '@/components/NameEvaluator/NameEvaluator.css';
+
+export default (props: any) => {
+  const { drawerOpen } = props;
+  const givenNameContext = useGivenNames();
+  const { givenNameCandidates } = givenNameContext.state;
+  const { approveCandidate, rejectCandidate, snoozeCandidate } = useGivenNamesActions();
+
+  const approveClick = async () => {
+    if (givenNameCandidates && givenNameCandidates.length > 0) {
+      await approveCandidate(givenNameCandidates[0].givenCustomNameBridgeId);
+    }
+  };
+
+  const rejectClick = async () => {
+    if (givenNameCandidates && givenNameCandidates.length > 0) {
+      await rejectCandidate(givenNameCandidates[0].givenCustomNameBridgeId);
+    }
+  };
+
+  const snoozeClick = async () => {
+    if (givenNameCandidates && givenNameCandidates.length > 0) {
+      await snoozeCandidate(givenNameCandidates[0].givenCustomNameBridgeId);
+    }
+  };
+
+  return (
+    <Box id="name-evaluator" className={drawerOpen ? 'drawer-open' : 'drawer-closed'}>
+      <p>{givenNameCandidates && givenNameCandidates.length > 0 ? givenNameCandidates[0].givenName : 'no names'}</p>
+      <p>
+        <button onClick={approveClick}>Approve</button>
+        <button onClick={rejectClick}>Reject</button>
+        <button onClick={snoozeClick}>Snooze</button>
+      </p>
+    </Box>
+  );
+};
--- src/pages/NameGenerator.tsx
+++ src/pages/NameGenerator.tsx
@@ -2,45 +2,17 @@ import { useGivenNames, useGivenNamesActions } from '@/state/givenName/givenName
 import { useFilters } from '@/state/filter/filter.context';
 import FilterDrawer from '@/components/FilterDrawer/FilterDrawer';
 import { useState } from 'react';
+import NameEvaluator from '@/components/NameEvaluator/NameEvaluator';
+import Box from '@mui/material/Box';
 
 const NameGenerator = () => {
-  const givenNameContext = useGivenNames();
-  const { approveCandidate, rejectCandidate, snoozeCandidate } = useGivenNamesActions();
-  const { givenNameCandidates } = givenNameContext.state;
-  const filterContext = useFilters();
-  const { decades } = filterContext.state;
   const [drawerOpen, setDrawerOpen] = useState(true);
-  console.log('🚀 ~ NameGenerator ~ drawerOpen:', drawerOpen);
-
-  const approveClick = async () => {
-    if (givenNameCandidates && givenNameCandidates.length > 0) {
-      await approveCandidate(givenNameCandidates[0].givenCustomNameBridgeId);
-    }
-  };
-
-  const rejectClick = async () => {
-    if (givenNameCandidates && givenNameCandidates.length > 0) {
-      await rejectCandidate(givenNameCandidates[0].givenCustomNameBridgeId);
-    }
-  };
-
-  const snoozeClick = async () => {
-    if (givenNameCandidates && givenNameCandidates.length > 0) {
-      await snoozeCandidate(givenNameCandidates[0].givenCustomNameBridgeId);
-    }
-  };
 
   return (
-    <>
+    <Box>
       <FilterDrawer drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
-      NameGenerator
-      <p>{givenNameCandidates && givenNameCandidates.length > 0 ? givenNameCandidates[0].givenName : 'no names'}</p>
-      <p>
-        <button onClick={approveClick}>Approve</button>
-        <button onClick={rejectClick}>Reject</button>
-        <button onClick={snoozeClick}>Snooze</button>
-      </p>
-    </>
+      <NameEvaluator drawerOpen={drawerOpen} />
+    </Box>
   );
 };
 
