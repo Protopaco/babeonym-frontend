4129eecb2bcd8477e0e1db5c460c592f59bdb106
Paul Stevens <paul.stevens.developer@gmail.com>
2026-08-19 15:32:10 -0700

    Remove console log and refactor approved names rendering in DrawerApprovedList

--- src/components/NameGenerator/DrawerApproved/DrawerApproved.tsx
+++ src/components/NameGenerator/DrawerApproved/DrawerApproved.tsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router-dom';
 export default () => {
   const navigate = useNavigate();
   const navToFullList = () => {
-    console.log('click');
     navigate('/list');
   };
   return (
