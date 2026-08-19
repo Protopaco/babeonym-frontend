a28700b1fd82bb3e91c4ca9ca361287e6c986410
Paul Stevens <paul.stevens.developer@gmail.com>
2026-08-19 16:48:54 -0700

    Refactor AccountLink component to use Link for navigation and update styles for consistency

--- src/components/Header/AccountLink/AccountLink.tsx
+++ src/components/Header/AccountLink/AccountLink.tsx
@@ -1,9 +1,10 @@
 import Typography from '@mui/material/Typography';
 import '@/components/Header/AccountLink/AccountLink.css';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <Typography variant="body1" color="primary" id="account-link">
+    <Typography variant="body1" color="primary" id="account-link" component={Link} to="/settings">
       Account
     </Typography>
   );
