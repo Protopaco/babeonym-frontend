be856c466fcaf2ec3723d10ec9dd5909bb00bc3d
Paul Stevens <paul.stevens.developer@gmail.com>
2026-08-19 16:39:33 -0700

    Refactor NavBar component to use NavLink for navigation and apply consistent styling to links

--- src/components/Header/NavBar/NavBar.tsx
+++ src/components/Header/NavBar/NavBar.tsx
@@ -1,15 +1,15 @@
 import { Typography } from '@mui/material';
 import '@/components/Header/NavBar/NavBar.css';
 import AppBar from '@mui/material/AppBar';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default () => {
   return (
     <AppBar position="static" id="nav-bar">
-      <Typography variant="h6" component={Link} to="/" sx={{ textDecoration: 'none', color: 'inherit' }}>
+      <Typography variant="h6" component={NavLink} to="/" className="nav-bar-link">
         Name Generator
       </Typography>
-      <Typography variant="h6" component={Link} to="/list" sx={{ textDecoration: 'none', color: 'inherit' }}>
+      <Typography variant="h6" component={NavLink} to="/list" className="nav-bar-link">
         Your Names
       </Typography>
     </AppBar>
