82a693d1f40beaf82a73ed6d34a09f74fdf3ecec
Paul Stevens <paul.stevens.developer@gmail.com>
2026-08-20 00:34:14 -0700

    Add scrollable Decades accordion and improve checkbox styling in FilterListItem

--- src/components/NameGenerator/FilterListItem/FilterListItem.tsx
+++ src/components/NameGenerator/FilterListItem/FilterListItem.tsx
@@ -16,7 +16,13 @@ type Props = {
 export default ({ index, label, action, selected }: Props) => {
   return (
     <ListItemButton className="filter-list-item" key={index} onClick={action}>
-      <ListItemIcon>{selected ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon />}</ListItemIcon>
+      <ListItemIcon>
+        {selected ? (
+          <CheckBoxIcon className="filter-list-item-checkbox filter-list-item-checkbox--checked" />
+        ) : (
+          <CheckBoxOutlineBlankIcon className="filter-list-item-checkbox" />
+        )}
+      </ListItemIcon>
       <ListItemText primary={label} />
     </ListItemButton>
   );
