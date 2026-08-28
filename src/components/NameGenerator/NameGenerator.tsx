f8187a2e762e4f679c85887ccce82b5aa6b2e2df
Paul Stevens <paul.stevens.developer@gmail.com>
2026-08-27 22:01:46 -0700

    Add NameGenerator CSS and implement useNameGeneratorUrlFilters for improved URL state management

--- src/components/NameGenerator/NameGenerator.tsx
+++ src/components/NameGenerator/NameGenerator.tsx
@@ -1,82 +1,24 @@
 import FilterDrawer from '@/components/NameGenerator/FilterDrawer/FilterDrawer';
-import { useGivenNames, useGivenNamesActions } from '@/state/givenName/givenName.provider';
-import { useState, useEffect } from 'react';
+import { useGivenNames } from '@/state/givenName/givenName.provider';
+import { useState } from 'react';
 import NameEvaluator from '@/components/NameGenerator/NameEvaluator/NameEvaluator';
 import Box from '@mui/material/Box';
-import { stringToGender } from '@/types/Gender';
-import type { Gender } from '@/types/Gender';
-import { useSearchParams } from 'react-router-dom';
 import MobileNameFilters from '@/components/NameGenerator/MobileNameFilters/MobileNameFilters';
+import { useNameGeneratorUrlFilters } from '@/components/NameGenerator/useNameGeneratorUrlFilters';
+import './NameGenerator.css';
 
 export default () => {
   const givenNameContext = useGivenNames();
-  const { addSelectedGenders, addSelectedDecadeIds, addSelectedLanguageIds, addSelectedCultureIds, getNewCandidates } = useGivenNamesActions();
   const { selectedCultureIds, selectedDecadeIds, selectedGenders, selectedLanguageIds, givenNameCandidates, givenNameProviderLoaded } =
     givenNameContext.state;
   const [drawerOpen, setDrawerOpen] = useState(true);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const filtersToUrlParams = () => {
-    const params = new URLSearchParams();
-
-    if (selectedGenders.length) params.append('genders', [...new Set(selectedGenders)].join(','));
-    if (selectedDecadeIds.length) params.append('decades', [...new Set(selectedDecadeIds)].join(','));
-    if (selectedLanguageIds.length) params.append('languages', [...new Set(selectedLanguageIds)].join(','));
-    if (selectedCultureIds.length) params.append('cultures', [...new Set(selectedCultureIds)].join(','));
-    setSearchParams(params, { replace: true });
-  };
-
-  const urlParamsToFilters = () => {
-    const genders: Gender[] | undefined = searchParams
-      .get('genders')
-      ?.split(',')
-      .map(stringToGender)
-      .filter((gender): gender is Gender => gender !== null);
-
-    const decades: number[] | undefined = searchParams
-      .get('decades')
-      ?.split(',')
-      .map(Number)
-      .filter((decadeId): number => decadeId);
-
-    const languages: number[] | undefined = searchParams
-      .get('languages')
-      ?.split(',')
-      .map(Number)
-      .filter((languageId): number => languageId);
-
-    const cultures: number[] | undefined = searchParams
-      .get('cultures')
-      ?.split(',')
-      .map(Number)
-      .filter((cultureId): number => cultureId);
-
-    return {
-      genders,
-      decades,
-      languages,
-      cultures,
-    };
-  };
-
-  useEffect(() => {
-    filtersToUrlParams();
-  }, [givenNameCandidates]);
-
-  useEffect(() => {
-    const updateCandidates = async (genders?: string[], decades?: number[], languages?: number[], cultures?: number[]) => {
-      await getNewCandidates(genders, decades, languages, cultures);
-    };
-    const { genders, decades, languages, cultures } = urlParamsToFilters();
-
-    if (genders) addSelectedGenders(genders);
-    if (decades) addSelectedDecadeIds(decades);
-    if (languages) addSelectedLanguageIds(languages);
-    if (cultures) addSelectedCultureIds(cultures);
-    if (genders || decades || languages || cultures) {
-      updateCandidates(genders, decades, languages, cultures);
-    }
-  }, []);
+  useNameGeneratorUrlFilters({
+    givenNameCandidates,
+    selectedCultureIds,
+    selectedDecadeIds,
+    selectedGenders,
+    selectedLanguageIds,
+  });
 
   return (
     <Box>
